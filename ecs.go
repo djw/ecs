199eac96643aadd3cd17a19cd5661860b69c6fc9
Dan Wilson <danjwilson@gmail.com>
2020-08-08 13:17:02 +0100

    Add information about services

--- ecs.go
+++ ecs.go
@@ -61,6 +61,66 @@ func getClusterDescriptions(svc *ecs.ECS, clusters []*string) (*ecs.DescribeClus
 	return result, nil
 }
 
+func getServices(svc *ecs.ECS, cluster *string) (*ecs.ListServicesOutput, error) {
+
+	result, err := svc.ListServices(&ecs.ListServicesInput{
+		Cluster: cluster,
+	})
+	if err != nil {
+		if aerr, ok := err.(awserr.Error); ok {
+			switch aerr.Code() {
+			case ecs.ErrCodeServerException:
+				fmt.Println(ecs.ErrCodeServerException, aerr.Error())
+			case ecs.ErrCodeClientException:
+				fmt.Println(ecs.ErrCodeClientException, aerr.Error())
+			case ecs.ErrCodeInvalidParameterException:
+				fmt.Println(ecs.ErrCodeInvalidParameterException, aerr.Error())
+			case ecs.ErrCodeClusterNotFoundException:
+				fmt.Println(ecs.ErrCodeClusterNotFoundException, aerr.Error())
+			default:
+				fmt.Println(aerr.Error())
+			}
+		} else {
+			// Print the error, cast err to awserr.Error to get the Code and
+			// Message from an error.
+			fmt.Println(err.Error())
+		}
+		return nil, err
+	}
+
+	return result, nil
+}
+
+func describeServices(svc *ecs.ECS, cluster *string, services []*string) (*ecs.DescribeServicesOutput, error) {
+	result, err := svc.DescribeServices(&ecs.DescribeServicesInput{
+		Cluster:  cluster,
+		Services: services,
+	})
+	if err != nil {
+		if aerr, ok := err.(awserr.Error); ok {
+			switch aerr.Code() {
+			case ecs.ErrCodeServerException:
+				fmt.Println(ecs.ErrCodeServerException, aerr.Error())
+			case ecs.ErrCodeClientException:
+				fmt.Println(ecs.ErrCodeClientException, aerr.Error())
+			case ecs.ErrCodeInvalidParameterException:
+				fmt.Println(ecs.ErrCodeInvalidParameterException, aerr.Error())
+			case ecs.ErrCodeClusterNotFoundException:
+				fmt.Println(ecs.ErrCodeClusterNotFoundException, aerr.Error())
+			default:
+				fmt.Println(aerr.Error())
+			}
+		} else {
+			// Print the error, cast err to awserr.Error to get the Code and
+			// Message from an error.
+			fmt.Println(err.Error())
+		}
+		return nil, err
+	}
+
+	return result, nil
+}
+
 func main() {
 	sess := session.Must(session.NewSessionWithOptions(session.Options{
 		SharedConfigState: session.SharedConfigEnable,
@@ -72,10 +132,19 @@ func main() {
 
 	const format = "%v\t%v\t%v\t\n"
 	tw := new(tabwriter.Writer).Init(os.Stdout, 0, 8, 2, ' ', 0)
-	fmt.Fprintf(tw, format, "Cluster", "Running", "Pending")
-	fmt.Fprintf(tw, format, "-------", "-------", "-------")
+	fmt.Fprintf(tw, format, "Cluster/Service", "Running", "Pending")
+	fmt.Fprintf(tw, format, "---------------", "-------", "-------")
 	for _, c := range clustersDescriptions.Clusters {
 		fmt.Fprintf(tw, format, *c.ClusterName, *c.RunningTasksCount, *c.PendingTasksCount)
+
+		clusterServices, _ := getServices(svc, c.ClusterArn)
+
+		if len(clusterServices.ServiceArns) > 0 {
+			clusterServiceDescriptions, _ := describeServices(svc, c.ClusterArn, clusterServices.ServiceArns)
+			for _, s := range clusterServiceDescriptions.Services {
+				fmt.Fprintf(tw, format, " - "+*s.ServiceName, *s.RunningCount, *s.PendingCount)
+			}
+		}
 	}
 	tw.Flush()
 }
